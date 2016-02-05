63138028782dcb73c3cd2b6037bcba0ce5c6fcf8
Francesco Pagetti <francesco.pagetti@westernpower.com.au>
2016-02-05 14:11:22 +0800

    let's get thing a bit more juicy

--- module-one/src/test/java/au/com/westernpower/moduleone/ModuleOneIntegrationTest.java
+++ module-one/src/test/java/au/com/westernpower/moduleone/ModuleOneIntegrationTest.java
@@ -1,5 +1,9 @@
 package au.com.westernpower.moduleone;
 
+import au.com.westernpower.ci.model.MyTBean;
+import au.com.westernpower.ci.repository.MyTBeanRepository;
+import au.com.westernpower.ci.repository.MyTBeanRepositoryImpl;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
@@ -9,9 +13,38 @@ import org.junit.experimental.categories.Category;
 @Category(au.com.westernpower.ci.IntegrationTest.class)
 public class ModuleOneIntegrationTest {
 
+    MyTBeanRepository repository = new MyTBeanRepositoryImpl();
+
     @Test
     public void test1(){
-        System.out.println("Running an integration test!");
+        MyTBean myTBean = repository.getInstance();
+
+        Assert.assertNotNull("My TBean should not be null",myTBean);
     }
 
+    @Test
+    public void test2(){
+        MyTBean myTBean = repository.getInstance();
+        myTBean.setName("TBean");
+        myTBean.setDescription("this is a bit of desc");
+
+        myTBean = repository.save(myTBean);
+
+        Assert.assertNotNull("My TBean should not be null",myTBean);
+        Assert.assertNotNull("ID should be not null",myTBean.getId());
+        Assert.assertNotNull("Description should not be null",myTBean.getDescription());
+    }
+
+    @Test
+    public void test3() {
+        MyTBean myTBean = repository.getInstance();
+        myTBean.setName("TBean");
+        myTBean.setDescription("this is a bit of desc");
+
+        myTBean = repository.save(myTBean);
+        Assert.assertNotNull("ID should be not null", myTBean.getId());
+
+        repository.delete(myTBean);
+        Assert.assertNull("ID should be null", myTBean.getId());
+    }
 }
---
+++ module-two/src/main/java/au/com/westernpower/ci/model/MyBean2.java
@@ -0,0 +1,35 @@
+package au.com.westernpower.ci.model;
+
+/**
+ * Created by N038603 on 5/02/2016.
+ */
+public class MyBean2 {
+
+    private String id;
+    private String username;
+    private String password;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
