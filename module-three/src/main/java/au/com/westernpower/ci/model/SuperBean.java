5987ff229941bbb4e5b7063971bb8bd350f80cc7
Francesco Pagetti <francesco.pagetti@westernpower.com.au>
2016-02-05 14:54:26 +0800

    module 3

---
+++ module-three/src/main/java/au/com/westernpower/ci/model/SuperBean.java
@@ -0,0 +1,26 @@
+package au.com.westernpower.ci.model;
+
+/**
+ * Created by N038603 on 5/02/2016.
+ */
+public class SuperBean {
+
+    private MyTBean bean;
+    private MyBean2 bean2;
+
+    public MyTBean getBean() {
+        return bean;
+    }
+
+    public void setBean(MyTBean bean) {
+        this.bean = bean;
+    }
+
+    public MyBean2 getBean2() {
+        return bean2;
+    }
+
+    public void setBean2(MyBean2 bean2) {
+        this.bean2 = bean2;
+    }
+}
