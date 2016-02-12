972c44a942e02ccf1cb1c27876d10ca0c3ac8205
pagio81 <francesco.pagetti@gmail.com>
2016-02-12 11:24:44 +0800

    sonar issues servlet

--- module-web/src/main/java/au/com/westernpower/ci/web/HelloWorldServlet.java
+++ module-web/src/main/java/au/com/westernpower/ci/web/HelloWorldServlet.java
@@ -3,6 +3,8 @@ package au.com.westernpower.ci.web;
 import au.com.westernpower.ci.model.SuperBean;
 import au.com.westernpower.ci.service.SuperBeanService;
 import au.com.westernpower.ci.service.SuperBeanServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -22,21 +24,27 @@ public class HelloWorldServlet extends HttpServlet {
     private static final String PAGE_HEADER = "<html><head><title>Hello World!</title></head><body>";
     private static final String PAGE_FOOTER = "</body></html>";
 
-    SuperBeanService service = new SuperBeanServiceImpl();
+    private transient final SuperBeanService service = new SuperBeanServiceImpl();
+    private static final Logger LOG = LoggerFactory.getLogger(HelloWorldServlet.class);
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
-        PrintWriter writer = resp.getWriter();
-        writer.println(PAGE_HEADER);
 
-        SuperBean bean = new SuperBean();
-        service.doSomething(bean);
+        try{
+            PrintWriter writer = resp.getWriter();
+            writer.println(PAGE_HEADER);
 
-        writer.println("<h1>Super Bean saved: " + bean.getId() + "</h1>");
+            SuperBean bean = new SuperBean();
+            service.doSomething(bean);
 
-        writer.println(PAGE_FOOTER);
-        writer.close();
+            writer.println("<h1>Super Bean saved: " + bean.getId() + "</h1>");
+
+            writer.println(PAGE_FOOTER);
+            writer.close();
+        } catch (IOException io){
+               LOG.error("I/O problem",io);
+        }
     }
 
 
